5a94ed58e78a2cc2f712375f54a9768f7e2ce1e5
BalashovM <mi-h-a@mail.ru>
2021-08-19 22:15:43 +0700

    Lesson02- Homework is done

---
+++ lesson2/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import InputMessageComponent from './InputMessageComponent';
+import MessageList from './MessageList';
+import './App.css';
+
+function App() {
+  const [inputMessage, setInputMessage] = useState('');
+  const [messageArray, setMessageArray] = useState([]);
+
+  const onSendMessage = () => {
+    if (inputMessage !== '') {
+      var message = {
+        text: inputMessage,
+        autor: 'Вы'
+      };
+      setMessageArray((prev) => [...prev, message]);
+      setInputMessage('');
+      setTimeout(addAnswer, 3000);
+    }
+  };
+
+
+  const addAnswer = () => {
+    var answer = {
+      autorAnswer: 'Бот',
+      textAnswer: 'Интересненько'
+    }
+    setMessageArray((prev) => [...prev, answer]); 
+  };
+  
+  return (
+    <div className="mainWrapper">
+      < MessageList
+        statMessageArray={messageArray}
+      />
+      < div className = 'inputWrapper' >
+        < InputMessageComponent
+          value = { inputMessage }
+          funcSetInputMessage={ setInputMessage } 
+          funcSetMessageArray={setMessageArray}
+          funcOnSendMessage={onSendMessage}
+        />
+        < button className = 'buttonSend' onClick = { onSendMessage } > Отправить </button> 
+      </div>
+    </div>
+  );
+};
+
+export default App;
